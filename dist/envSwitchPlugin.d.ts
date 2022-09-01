e99a92e7e0a832d949c342f7918d6d3890f4b6ad
pengbo43 <pengbo43@jd.com>
2022-09-01 13:56:20 +0800

    chore: build

--- dist/envSwitchPlugin.d.ts
+++ dist/envSwitchPlugin.d.ts
@@ -1,7 +1,10 @@
 import { PluginOption } from 'vite';
 export interface PluginConfig {
     root: string;
+    envs: string[];
+    wsPath: string;
     eventName?: string;
+    wsProtocol?: string;
     beforeRestart?: (server: any, newServer: any) => void;
 }
 export declare const envSwitchPlugin: (pluginConfig: PluginConfig) => PluginOption;
