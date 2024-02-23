2778048f97b474585afea5b669f3c9cc9725adad
pengbo43 <pengbo43@jd.com>
2024-02-23 15:11:31 +0800

    release: 1.3.0

--- dist/envSwitchPlugin.js
+++ dist/envSwitchPlugin.js
@@ -2,22 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.envSwitchPlugin = void 0;
 const vite_1 = require("vite");
-const path_1 = require("path");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenv = require('dotenv');
-// 获取环境变量
-function loadEnv(path, mode) {
-    const basePath = (0, path_1.resolve)(path, `.env${mode ? `.${mode}` : ``}`);
-    const localPath = `${basePath}.local`;
-    const load = (envPath) => {
-        const env = dotenv.config({ path: envPath, debug: process.env.DEBUG });
-        process.env = Object.assign(Object.assign({}, process.env), env.parsed);
-    };
-    load(localPath);
-    load(basePath);
-}
 const envSwitchPlugin = (pluginConfig) => {
-    const { beforeRestart, eventName = 'env-switch', root, wsProtocol = 'vite-hmr', wsPath, envs = [], } = pluginConfig;
+    const { beforeRestart, eventName = 'env-switch', wsProtocol = 'vite-hmr', wsPath, envs = [], } = pluginConfig;
     let initMode = '';
     return {
         enforce: 'post',
@@ -31,10 +17,6 @@ const envSwitchPlugin = (pluginConfig) => {
                 const newServer = await (0, vite_1.createServer)(Object.assign({}, Object.assign({}, config.inlineConfig), { mode: env }));
                 newServer.config.server.open = false;
                 await server.close();
-                // 重新获取环境变量
-                loadEnv(root, env);
-                // 兼容 process
-                newServer.config.define['process.env'] = process.env;
                 if (beforeRestart) {
                     await beforeRestart(server, newServer);
                 }
@@ -92,7 +74,7 @@ const envSwitchPlugin = (pluginConfig) => {
                             injectTo: 'head',
                             children: envs.length
                                 ? `
-                .env-btn-wrapper .env-btn{
+                .env-btn-wrapper .env-btn {
                   background-color: pink;
                   color: red;
                   border-radius: 4px;
@@ -102,6 +84,7 @@ const envSwitchPlugin = (pluginConfig) => {
                   position: fixed;
                   bottom: 0.7rem;
                   right: 0.2rem;
+                  z-index: 100000;
                 }
               `
                                 : '',
